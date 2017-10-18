339df4018640955b1814ed50535a6e6861cc6925
PsyMan47 <xhonirexhepi@gmail.com>
2017-10-18 22:20:46 +0200

    Make it ready for LOS

--- builder.sh
+++ builder.sh
@@ -47,10 +47,11 @@ DTBTOOL=$KERNEL_DIR/dtbTool
 DATE=$(date +"%d%m%Y")
 KERNEL_NAME="Psychedelic-Kernel"
 DEVICE="-capricorn-"
-VER="-v0.2"
-FINAL_ZIP="$KERNEL_NAME""$DEVICE""$DATE""$VER".zip
+VER="-v0.3-"
+TYPE="CM"
+FINAL_ZIP="$KERNEL_NAME""$DEVICE""$DATE""$VER""$TYPE".zip
 
-rm -rf $ANYKERNEL_DIR/capricorn/*.ko && rm $ANYKERNEL_DIR/capricorn/zImage $ANYKERNEL_DIR/capricorn/dtb
+rm $ANYKERNEL_DIR/capricorn/zImage $ANYKERNEL_DIR/capricorn/dtb
 rm $KERNEL_DIR/arch/arm64/boot/Image.gz $KERNEL_DIR/arch/arm64/boot/dt.img $KERNEL_DIR/arch/arm64/boot/Image.gz-dtb
 
 export ARCH=arm64
@@ -59,7 +60,7 @@ export KBUILD_BUILD_HOST="PsyBuntu"
 export CROSS_COMPILE=/home/psybuntu/toolchain/aarch64-linux-android-5.3/bin/aarch64-linux-android-
 export LD_LIBRARY_PATH=/home/psybuntu/toolchain/aarch64-linux-android-5.3/lib/
 export USE_CCACHE=1
-export CCACHE_DIR=/home/psybuntu/DragonXia-OC/CCACHES/capricorn/.ccache
+export CCACHE_DIR=/home/psybuntu/DragonXia-OC-LOS/CCACHES/capricorn/.ccache
 
 make clean && make mrproper
 make capricorn_defconfig
@@ -68,17 +69,6 @@ make -j3
 ./dtbTool -s 2048 -o arch/arm64/boot/dt.img -p scripts/dtc/ arch/arm/boot/dts/qcom/
 cp $KERNEL_DIR/arch/arm64/boot/dt.img $ANYKERNEL_DIR/capricorn/dtb
 cp $KERNEL_DIR/arch/arm64/boot/Image.gz $ANYKERNEL_DIR/capricorn/zImage
-cp $KERNEL_DIR/drivers/staging/qcacld-2.0/wlan.ko $ANYKERNEL_DIR/capricorn/
-cp $KERNEL_DIR/drivers/char/hw_random/msm_rng.ko $ANYKERNEL_DIR/capricorn/
-cp $KERNEL_DIR/drivers/char/hw_random/rng-core.ko $ANYKERNEL_DIR/capricorn/
-cp $KERNEL_DIR/drivers/char/rdbg.ko $ANYKERNEL_DIR/capricorn/
-cp $KERNEL_DIR/drivers/spi/spidev.ko $ANYKERNEL_DIR/capricorn/
-cp $KERNEL_DIR/drivers/input/evbug.ko $ANYKERNEL_DIR/capricorn/
-cp $KERNEL_DIR/crypto/ansi_cprng.ko $ANYKERNEL_DIR/capricorn/
-cp $KERNEL_DIR/drivers/mmc/card/mmc_test.ko $ANYKERNEL_DIR/capricorn/
-cp $KERNEL_DIR/drivers/video/backlight/lcd.ko $ANYKERNEL_DIR/capricorn/
-cp $KERNEL_DIR/drivers/video/backlight/backlight.ko $ANYKERNEL_DIR/capricorn/
-cp $KERNEL_DIR/drivers/video/backlight/generic_bl.ko $ANYKERNEL_DIR/capricorn/
 cd $ANYKERNEL_DIR/capricorn
 zip -r9 $FINAL_ZIP * -x *.zip $FINAL_ZIP
       ;;
@@ -89,10 +79,11 @@ DTBTOOL=$KERNEL_DIR/dtbTool
 DATE=$(date +"%d%m%Y")
 KERNEL_NAME="Psychedelic-Kernel"
 DEVICE="-natrium-"
-VER="-v0.2"
-FINAL_ZIP="$KERNEL_NAME""$DEVICE""$DATE""$VER".zip
+VER="-v0.3-"
+TYPE="CM"
+FINAL_ZIP="$KERNEL_NAME""$DEVICE""$DATE""$VER""$TYPE".zip
 
-rm -rf $ANYKERNEL_DIR/natrium/*.ko && rm $ANYKERNEL_DIR/natrium/zImage $ANYKERNEL_DIR/natrium/dtb
+rm $ANYKERNEL_DIR/natrium/zImage $ANYKERNEL_DIR/natrium/dtb
 rm $KERNEL_DIR/arch/arm64/boot/Image.gz $KERNEL_DIR/arch/arm64/boot/dt.img $KERNEL_DIR/arch/arm64/boot/Image.gz-dtb
 
 export ARCH=arm64
@@ -101,7 +92,7 @@ export KBUILD_BUILD_HOST="PsyBuntu"
 export CROSS_COMPILE=/home/psybuntu/toolchain/aarch64-linux-android-5.3/bin/aarch64-linux-android-
 export LD_LIBRARY_PATH=/home/psybuntu/toolchain/aarch64-linux-android-5.3/lib/
 export USE_CCACHE=1
-export CCACHE_DIR=/home/psybuntu/DragonXia-OC/CCACHES/natrium/.ccache
+export CCACHE_DIR=/home/psybuntu/DragonXia-OC-LOS/CCACHES/natrium/.ccache
 
 make clean && make mrproper
 make natrium_defconfig
@@ -110,17 +101,6 @@ make -j3
 ./dtbTool -s 2048 -o arch/arm64/boot/dt.img -p scripts/dtc/ arch/arm/boot/dts/qcom/
 cp $KERNEL_DIR/arch/arm64/boot/dt.img $ANYKERNEL_DIR/natrium/dtb
 cp $KERNEL_DIR/arch/arm64/boot/Image.gz $ANYKERNEL_DIR/natrium/zImage
-cp $KERNEL_DIR/drivers/staging/qcacld-2.0/wlan.ko $ANYKERNEL_DIR/natrium/
-cp $KERNEL_DIR/drivers/char/hw_random/msm_rng.ko $ANYKERNEL_DIR/natrium/
-cp $KERNEL_DIR/drivers/char/hw_random/rng-core.ko $ANYKERNEL_DIR/natrium/
-cp $KERNEL_DIR/drivers/char/rdbg.ko $ANYKERNEL_DIR/natrium/
-cp $KERNEL_DIR/drivers/spi/spidev.ko $ANYKERNEL_DIR/natrium/
-cp $KERNEL_DIR/drivers/input/evbug.ko $ANYKERNEL_DIR/natrium/
-cp $KERNEL_DIR/crypto/ansi_cprng.ko $ANYKERNEL_DIR/natrium/
-cp $KERNEL_DIR/drivers/mmc/card/mmc_test.ko $ANYKERNEL_DIR/natrium/
-cp $KERNEL_DIR/drivers/video/backlight/lcd.ko $ANYKERNEL_DIR/natrium/
-cp $KERNEL_DIR/drivers/video/backlight/backlight.ko $ANYKERNEL_DIR/natrium/
-cp $KERNEL_DIR/drivers/video/backlight/generic_bl.ko $ANYKERNEL_DIR/natrium/
 cd $ANYKERNEL_DIR/natrium
 zip -r9 $FINAL_ZIP * -x *.zip $FINAL_ZIP
       ;;
@@ -131,10 +111,11 @@ DTBTOOL=$KERNEL_DIR/dtbTool
 DATE=$(date +"%d%m%Y")
 KERNEL_NAME="Psychedelic-Kernel"
 DEVICE="-lithium-"
-VER="-v0.2"
-FINAL_ZIP="$KERNEL_NAME""$DEVICE""$DATE""$VER".zip
+VER="-v0.3-"
+TYPE="CM"
+FINAL_ZIP="$KERNEL_NAME""$DEVICE""$DATE""$VER""$TYPE".zip
 
-rm -rf $ANYKERNEL_DIR/lithium/*.ko && rm $ANYKERNEL_DIR/lithium/zImage $ANYKERNEL_DIR/lithium/dtb
+rm $ANYKERNEL_DIR/lithium/zImage $ANYKERNEL_DIR/lithium/dtb
 rm $KERNEL_DIR/arch/arm64/boot/Image.gz $KERNEL_DIR/arch/arm64/boot/dt.img $KERNEL_DIR/arch/arm64/boot/Image.gz-dtb
 
 export ARCH=arm64
@@ -143,7 +124,7 @@ export KBUILD_BUILD_HOST="PsyBuntu"
 export CROSS_COMPILE=/home/psybuntu/toolchain/aarch64-linux-android-5.3/bin/aarch64-linux-android-
 export LD_LIBRARY_PATH=/home/psybuntu/toolchain/aarch64-linux-android-5.3/lib/
 export USE_CCACHE=1
-export CCACHE_DIR=/home/psybuntu/DragonXia-OC/CCACHES/lithium/.ccache
+export CCACHE_DIR=/home/psybuntu/DragonXia-OC-LOS/CCACHES/lithium/.ccache
 
 make clean && make mrproper
 make lithium_defconfig
@@ -152,17 +133,6 @@ make -j3
 ./dtbTool -s 2048 -o arch/arm64/boot/dt.img -p scripts/dtc/ arch/arm/boot/dts/qcom/
 cp $KERNEL_DIR/arch/arm64/boot/dt.img $ANYKERNEL_DIR/lithium/dtb
 cp $KERNEL_DIR/arch/arm64/boot/Image.gz $ANYKERNEL_DIR/lithium/zImage
-cp $KERNEL_DIR/drivers/staging/qcacld-2.0/wlan.ko $ANYKERNEL_DIR/lithium/
-cp $KERNEL_DIR/drivers/char/hw_random/msm_rng.ko $ANYKERNEL_DIR/lithium/
-cp $KERNEL_DIR/drivers/char/hw_random/rng-core.ko $ANYKERNEL_DIR/lithium/
-cp $KERNEL_DIR/drivers/char/rdbg.ko $ANYKERNEL_DIR/lithium/
-cp $KERNEL_DIR/drivers/spi/spidev.ko $ANYKERNEL_DIR/lithium/
-cp $KERNEL_DIR/drivers/input/evbug.ko $ANYKERNEL_DIR/lithium/
-cp $KERNEL_DIR/crypto/ansi_cprng.ko $ANYKERNEL_DIR/lithium/
-cp $KERNEL_DIR/drivers/mmc/card/mmc_test.ko $ANYKERNEL_DIR/lithium/
-cp $KERNEL_DIR/drivers/video/backlight/lcd.ko $ANYKERNEL_DIR/lithium/
-cp $KERNEL_DIR/drivers/video/backlight/backlight.ko $ANYKERNEL_DIR/lithium/
-cp $KERNEL_DIR/drivers/video/backlight/generic_bl.ko $ANYKERNEL_DIR/lithium/
 cd $ANYKERNEL_DIR/lithium/
 zip -r9 $FINAL_ZIP * -x *.zip $FINAL_ZIP
       ;;
@@ -173,10 +143,11 @@ DTBTOOL=$KERNEL_DIR/dtbTool
 DATE=$(date +"%d%m%Y")
 KERNEL_NAME="Psychedelic-Kernel"
 DEVICE="-scorpio-"
-VER="-v0.2"
-FINAL_ZIP="$KERNEL_NAME""$DEVICE""$DATE""$VER".zip
+VER="-v0.3-"
+TYPE="CM"
+FINAL_ZIP="$KERNEL_NAME""$DEVICE""$DATE""$VER""$TYPE".zip
 
-rm -rf $ANYKERNEL_DIR/scorpio/*.ko && rm $ANYKERNEL_DIR/scorpio/zImage $ANYKERNEL_DIR/scorpio/dtb
+rm $ANYKERNEL_DIR/scorpio/zImage $ANYKERNEL_DIR/scorpio/dtb
 rm $KERNEL_DIR/arch/arm64/boot/Image.gz $KERNEL_DIR/arch/arm64/boot/dt.img $KERNEL_DIR/arch/arm64/boot/Image.gz-dtb
 
 export ARCH=arm64
@@ -185,7 +156,7 @@ export KBUILD_BUILD_HOST="PsyBuntu"
 export CROSS_COMPILE=/home/psybuntu/toolchain/aarch64-linux-android-5.3/bin/aarch64-linux-android-
 export LD_LIBRARY_PATH=/home/psybuntu/toolchain/aarch64-linux-android-5.3/lib/
 export USE_CCACHE=1
-export CCACHE_DIR=/home/psybuntu/DragonXia-OC/CCACHES/scorpio/.ccache
+export CCACHE_DIR=/home/psybuntu/DragonXia-OC-LOS/CCACHES/scorpio/.ccache
 
 make clean && make mrproper
 make scorpio_defconfig
@@ -194,17 +165,6 @@ make -j3
 ./dtbTool -s 2048 -o arch/arm64/boot/dt.img -p scripts/dtc/ arch/arm/boot/dts/qcom/
 cp $KERNEL_DIR/arch/arm64/boot/dt.img $ANYKERNEL_DIR/scorpio/dtb
 cp $KERNEL_DIR/arch/arm64/boot/Image.gz $ANYKERNEL_DIR/scorpio/zImage
-cp $KERNEL_DIR/drivers/staging/qcacld-2.0/wlan.ko $ANYKERNEL_DIR/scorpio/
-cp $KERNEL_DIR/drivers/char/hw_random/msm_rng.ko $ANYKERNEL_DIR/scorpio/
-cp $KERNEL_DIR/drivers/char/hw_random/rng-core.ko $ANYKERNEL_DIR/scorpio/
-cp $KERNEL_DIR/drivers/char/rdbg.ko $ANYKERNEL_DIR/scorpio/
-cp $KERNEL_DIR/drivers/spi/spidev.ko $ANYKERNEL_DIR/scorpio/
-cp $KERNEL_DIR/drivers/input/evbug.ko $ANYKERNEL_DIR/scorpio/
-cp $KERNEL_DIR/crypto/ansi_cprng.ko $ANYKERNEL_DIR/scorpio/
-cp $KERNEL_DIR/drivers/mmc/card/mmc_test.ko $ANYKERNEL_DIR/scorpio/
-cp $KERNEL_DIR/drivers/video/backlight/lcd.ko $ANYKERNEL_DIR/scorpio/
-cp $KERNEL_DIR/drivers/video/backlight/backlight.ko $ANYKERNEL_DIR/scorpio/
-cp $KERNEL_DIR/drivers/video/backlight/generic_bl.ko $ANYKERNEL_DIR/scorpio/
 cd $ANYKERNEL_DIR/scorpio
 zip -r9 $FINAL_ZIP * -x *.zip $FINAL_ZIP
       ;;
@@ -215,10 +175,11 @@ DTBTOOL=$KERNEL_DIR/dtbTool
 DATE=$(date +"%d%m%Y")
 KERNEL_NAME="Psychedelic-Kernel"
 DEVICE="-gemini-"
-VER="-v0.2"
-FINAL_ZIP="$KERNEL_NAME""$DEVICE""$DATE""$VER".zip
+VER="-v0.3-"
+TYPE="CM"
+FINAL_ZIP="$KERNEL_NAME""$DEVICE""$DATE""$VER""$TYPE".zip
 
-rm -rf $ANYKERNEL_DIR/gemini/*.ko && rm $ANYKERNEL_DIR/gemini/zImage $ANYKERNEL_DIR/gemini/dtb
+rm $ANYKERNEL_DIR/gemini/zImage $ANYKERNEL_DIR/gemini/dtb
 rm $KERNEL_DIR/arch/arm64/boot/Image.gz $KERNEL_DIR/arch/arm64/boot/dt.img $KERNEL_DIR/arch/arm64/boot/Image.gz-dtb
 
 export ARCH=arm64
@@ -227,7 +188,7 @@ export KBUILD_BUILD_HOST="PsyBuntu"
 export CROSS_COMPILE=/home/psybuntu/toolchain/aarch64-linux-android-5.3/bin/aarch64-linux-android-
 export LD_LIBRARY_PATH=/home/psybuntu/toolchain/aarch64-linux-android-5.3/lib/
 export USE_CCACHE=1
-export CCACHE_DIR=/home/psybuntu/DragonXia-OC/CCACHES/gemini/.ccache
+export CCACHE_DIR=/home/psybuntu/DragonXia-OC-LOS/CCACHES/gemini/.ccache
 
 make clean && make mrproper
 make gemini_defconfig
@@ -236,17 +197,6 @@ make -j3
 ./dtbTool -s 2048 -o arch/arm64/boot/dt.img -p scripts/dtc/ arch/arm/boot/dts/qcom/
 cp $KERNEL_DIR/arch/arm64/boot/dt.img $ANYKERNEL_DIR/gemini/dtb
 cp $KERNEL_DIR/arch/arm64/boot/Image.gz $ANYKERNEL_DIR/gemini/zImage
-cp $KERNEL_DIR/drivers/staging/qcacld-2.0/wlan.ko $ANYKERNEL_DIR/gemini/
-cp $KERNEL_DIR/drivers/char/hw_random/msm_rng.ko $ANYKERNEL_DIR/gemini/
-cp $KERNEL_DIR/drivers/char/hw_random/rng-core.ko $ANYKERNEL_DIR/gemini/
-cp $KERNEL_DIR/drivers/char/rdbg.ko $ANYKERNEL_DIR/gemini/
-cp $KERNEL_DIR/drivers/spi/spidev.ko $ANYKERNEL_DIR/gemini/
-cp $KERNEL_DIR/drivers/input/evbug.ko $ANYKERNEL_DIR/gemini/
-cp $KERNEL_DIR/crypto/ansi_cprng.ko $ANYKERNEL_DIR/gemini/
-cp $KERNEL_DIR/drivers/mmc/card/mmc_test.ko $ANYKERNEL_DIR/gemini/
-cp $KERNEL_DIR/drivers/video/backlight/lcd.ko $ANYKERNEL_DIR/gemini/
-cp $KERNEL_DIR/drivers/video/backlight/backlight.ko $ANYKERNEL_DIR/gemini/
-cp $KERNEL_DIR/drivers/video/backlight/generic_bl.ko $ANYKERNEL_DIR/gemini/
 cd $ANYKERNEL_DIR/gemini
 zip -r9 $FINAL_ZIP * -x *.zip $FINAL_ZIP
       ;;
